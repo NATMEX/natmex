fd86935988e1c959a51f0ea3c7460ecd6d4a953f
NATMEX <pa_sanook.com@hotmail.com>
2019-07-01 15:27:58 +0700

    images OK

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' as prefix0;
+
+//Ctrl+Alt+F=จัดระเบียบ
+//flutter_run=run
+//View>Terminal=เปิดกล่องใส่คำสั่ง
 
 class Authen extends StatefulWidget {
   @override
@@ -6,8 +11,27 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  Widget showLogo() {
+    return Image.asset(
+      'images/logo.png',
+      fit: BoxFit.contain,
+    ); //ใส่รูปภาพ
+  }
+
+  Widget showText() {
+    return Text('Authen'); //ใส่ตัวหนังสือ
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Authen');
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(top: 60.0), //ห่างจากด้านบน
+        alignment: Alignment.topCenter, //ไว้ตรงกลาง
+        child: Column(
+          children: <Widget>[showLogo(), showText()], //แสดงผลเป็นบรรทัด
+        ),
+      ),
+    );
   }
 }
